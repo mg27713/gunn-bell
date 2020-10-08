da9a425e477b0fcd083f4e7a9be5fbdfadc8d86d
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-10-07 19:57:24 -0700

    So Firefox lied to me...thank you for being helpful in the console at least!

--- sw-test-2/sw-loader.js
+++ sw-test-2/sw-loader.js
@@ -1,10 +1,11 @@
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register("./sw.js");
 	
-	await import("./versions.js"); // after service worker loads so the transformation happens.
-	
-	if (LATEST_VERSION != CURRENT_VERSION) {
-		navigator.serviceWorker.getRegistrations().then(rs => rs.map(r => r.unregister()));
-		location.reload();
-	}
+	(await import("./versions.js")) // after service worker loads so the transformation happens.
+		.then(() => {
+			if (LATEST_VERSION != CURRENT_VERSION) {
+				navigator.serviceWorker.getRegistrations().then(rs => rs.map(r => r.unregister()));
+				location.reload();
+			}
+		});
 }
