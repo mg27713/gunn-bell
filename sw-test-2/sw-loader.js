cb6ba806199124b6f0cf1216ee6668672b034307
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-10-07 19:58:20 -0700

    Wow, I'm very stupid here.

--- sw-test-2/sw-loader.js
+++ sw-test-2/sw-loader.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register("./sw.js");
 	
-	(await import("./versions.js")) // after service worker loads so the transformation happens.
+	import("./versions.js") // after service worker loads so the transformation happens.
 		.then(() => {
 			if (LATEST_VERSION != CURRENT_VERSION) {
 				navigator.serviceWorker.getRegistrations().then(rs => rs.map(r => r.unregister()));
