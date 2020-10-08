183d28f72ca6902734c8fd2c9a43549565ce39d2
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-10-07 22:22:17 -0700

    Common error handling in the service worker loader

--- sw-test-2/sw-loader.js
+++ sw-test-2/sw-loader.js
@@ -14,5 +14,10 @@ if ("serviceWorker" in navigator) {
 				navigator.serviceWorker.getRegistrations().then(rs => rs.map(r => r.unregister()));
 				location.reload();
 			}
+		})
+		.catch(e => {
+			if (!(e instanceof TypeError)) // TypeError happens on the first load beecause the service worker hasn't replaced the macros in versions.js
+				// but if the service worker is first being installed, then it must be up to date.
+				throw e;
 		});
 }
