4129639649f46cc9521400b10729529c57bbd85e
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-10-07 19:49:03 -0700

    Small import issue fixed.

--- sw-test-2/sw-loader.js
+++ sw-test-2/sw-loader.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register("./sw.js");
 	
-	import("versions.js"); // after service worker loads so the transformation happens.
+	import("./versions.js"); // after service worker loads so the transformation happens.
 	
 	if (LATEST_VERSION != CURRENT_VERSION) {
 		navigator.serviceWorker.getRegistrations().then(rs => rs.map(r => r.unregister()));
