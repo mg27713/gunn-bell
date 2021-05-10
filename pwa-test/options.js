c24e1d3f2a1a791affd8967071bd62328955c1c9
mg27713 <68036487+mg27713@users.noreply.github.com>
2021-05-09 21:16:13 -0700

    Copied over stability improvements

--- pwa-test/options.js
+++ pwa-test/options.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function() {
 	const audio = document.getElementById("bell-sound");
+	const testAudio = document.getElementById("test-sound");
 	
 	const volume = document.getElementById("volume-slider");
 	const volumeField = document.getElementById("volume-display");
@@ -7,13 +8,15 @@ window.addEventListener("load", function() {
 	if (localStorage.settingVolume)
 		volume.value = localStorage.settingVolume;
 	
-	audio.volume = parseInt(volume.value/100);
+	audio.volume = parseInt(volume.value)/100;
+	testAudio.volume = audio.volume;
 	volumeField.innerHTML = volume.value;
 	
 	volume.addEventListener("change", function() {
 //		console.log("new volume: " + volume.value);
 		
 		audio.volume = parseInt(volume.value)/100;
+		testAudio.volume = audio.volume;
 		volumeField.innerHTML = volume.value;
 		localStorage.settingVolume = volume.value;
 	});
